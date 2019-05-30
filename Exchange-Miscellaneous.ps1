eef381d1ced9a39bc359c985ac42187c5bb69e21
Tuertolin <gaston@sarli.co>
2019-05-30 21:36:52 +1000

    Update Exchange-Miscellaneous.ps1
    Co-Authored-By: tuertolin <tuertolin@users.noreply.github.com>

--- Exchange-Miscellaneous.ps1
+++ Exchange-Miscellaneous.ps1
@@ -8,3 +8,7 @@ Set-Mailbox "Dan Jump" -EmailAddresses @{add="dan.jump@northamerica.contoso.com"
 # Repair mailbox
 New-MailboxRepairRequest -Mailbox user -CorruptionType ProvisionedFolder,SearchFolder,AggregateCounts,Folderview
 
+# Export a mailbox to a PST
+New-MailboxExportRequest -Mailbox harry.potter@contoso.com -FilePath \\SHARE-FILE-Server\UsersExport\harry.potter.pst
+
+Get-MailboxExportRequest
